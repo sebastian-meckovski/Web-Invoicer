fe0bc5e554c62b07636489a3f29727cc5a8c819e
sebastian <sebastian.meckovski@gmail.com>
2021-03-04 15:26:22 +0100

    further development of app. POST and GET methods added

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        print("this is what we get:  ", entry_content)
+
+
     return render_template("home.html")
 
+if __name__ == '__main__':
+    app.run()
