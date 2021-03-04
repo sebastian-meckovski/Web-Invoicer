25121c874e2f941e0d431c6cb0ab1ac939d7dba9
sebastian <sebastian.meckovski@gmail.com>
2021-03-04 22:04:16 +0100

    retrieved data now can be seen on the PDF document

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from invoicingCopy2 import run_stuff
+from SampleData import sample_list
 
 app = Flask(__name__)
 
@@ -6,41 +8,46 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method == "POST":
-       first_name = request.form.get("firstname")
-       last_name = request.form.get("lasttname")
-       adress_line1 = request.form.get("adress_line1")
-       adress_line2 = request.form.get("adress_line2")
+       full_name = request.form.get("firstname")
+       adress_line = request.form.get("lasttname")
+       city = request.form.get("city")
+       country = request.form.get("country")
        zip_postal = request.form.get("zip_postal")
 
-       first_name_client = request.form.get("firstnameclient")
-       last_name_client = request.form.get("lasttnameclient")
-       adress_line1_client = request.form.get("adress_line1_client")
-       adress_line2_client = request.form.get("adress_line2_client")
+       full_name_client = request.form.get("firstnameclient")
+       adress_line_client = request.form.get("lasttnameclient")
+       city_client = request.form.get("city_client")
+       country_client = request.form.get("country_client")
        zip_postal_client = request.form.get("zip_postal_client")
 
        item_name = request.form.get("item_name")
        quantity = request.form.get("qty")
        rate = request.form.get("rate")
 
-       print("Name:", first_name, "\n",
-             "lasttname:", last_name, "\n",
-             "AdressLine1:", adress_line1, "\n",
-             "AdressLine2:", adress_line2, "\n",
+       print("Name:", full_name, "\n",
+             "lasttname:", adress_line, "\n",
+             "AdressLine1:", city, "\n",
+             "AdressLine2:", country, "\n",
              "postal code:", zip_postal, "\n")
  
        
-       print("client first Name:", first_name_client, "\n",
-             "client lasttname:", last_name_client, "\n",
-             "client AdressLine1:", adress_line1_client, "\n",
-             "client AdressLine2:", adress_line2_client, "\n",
+       print("client first Name:", full_name_client, "\n",
+             "client lasttname:", adress_line_client, "\n",
+             "client AdressLine1:", city_client, "\n",
+             "client AdressLine2:", country_client, "\n",
              "client postal code:", zip_postal_client, "\n")
 
        print("item name:", item_name, "\n",
              "Qantity:", quantity, "\n",
-              "Rate:", rate, "\n")
+             "Rate:", rate, "\n")
+      
+       run_stuff(sample_list, full_name=full_name, adress_line=adress_line)
+
 
 
     return render_template("home.html")
+
+
 
 if __name__ == '__main__':
     app.run()
